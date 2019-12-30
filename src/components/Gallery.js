8359f43fb3cdf8c97e0d98d1374e0b4300ded1f9
Vinesma <otaviocos_13@hotmail.com>
2019-12-30 15:47:03 -0300

    +Added state to Gallery.js +CSS changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Gallery() {
-    return (
-        <section className="gallery-section">
-            <div className="gallery-section-nav">
-                <button className="nav-button">
-                    <i class="fas fa-chevron-circle-up" />
-                    Collapse All
-                </button>
-                <button className="nav-button">
-                    <i class="fas fa-chevron-circle-down" />
-                    Expand All
-                </button>
-            </div>
-        </section>
-    )
+class Gallery extends Component {
+    state = {
+        categories: [
+            {
+                name: 'Wallpapers',
+                icon: '',
+            },
+            {
+                name: 'Vectors',
+                icon: '',
+            },
+            {
+                name: 'Pixel Art',
+                icon: '',
+            },
+            {
+                name: 'Traditional',
+                icon: '',
+            },
+            {
+                name: 'Icons & Logos',
+                icon: '',
+            },
+            {
+                name: 'Banners',
+                icon: '',
+            },
+        ],
+    }
+
+    render(){
+        return (
+            <section className="gallery-section">
+                <div className="gallery-section-nav">
+                    <button className="bold-text-button">
+                        <i class="fas fa-chevron-circle-up" />
+                        Collapse All
+                    </button>
+                    <button className="bold-text-button">
+                        <i class="fas fa-chevron-circle-down" />
+                        Expand All
+                    </button>
+                    { this.state.categories.map(category => (
+                        <button className="nav-button">
+                            { category.icon === '' ? null : <i class={category.icon} />}                            
+                            { category.name }
+                        </button>
+                        )) 
+                    }
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Gallery
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,24 +8,24 @@ function Header(props) {
             <h4>{props.headerTitle}</h4>
             </div>
             <div className="main-header-options">
-                    { props.headerItems.map((item, index) => (
-                        <Link key={item.name} to={item.name.toLowerCase()}>
-                            <button 
-                            className={ item.active ? 'nav-button-active' : 'nav-button'}
-                            onClick={() => props.updateHeaderItems(item.name, index)}
-                            >
-                                <i className={item.icon} />
-                                {item.name === '/' ? 'Home' : item.name}
-                            </button>
-                        </Link>
-                    ))
-                    }
-                    <a href="mailto:vinesma.work@gmail.com">
-                        <button className="nav-button">
-                            <i className="fas fa-envelope" />
-                            Contact
+                { props.headerItems.map((item, index) => (
+                    <Link key={item.name} to={item.name.toLowerCase()}>
+                        <button 
+                        className={ item.active ? 'nav-button-active' : 'nav-button'}
+                        onClick={() => props.updateHeaderItems(item.name, index)}
+                        >
+                            <i className={item.icon} />
+                            {item.name === '/' ? 'Home' : item.name}
                         </button>
-                    </a>
+                    </Link>
+                ))
+                }
+                <a href="mailto:vinesma.work@gmail.com">
+                    <button className="nav-button">
+                        <i className="fas fa-envelope" />
+                        Contact
+                    </button>
+                </a>
             </div>
         </header>
     )
