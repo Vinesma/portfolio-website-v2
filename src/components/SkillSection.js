2321a44c51dddcc4e26293195e8a3651df188209
Vinesma <otaviocos_13@hotmail.com>
2020-01-01 20:22:47 -0300

    +Skills page +Mobile Support

---
+++ src/components/SkillSection.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function SkillSection(props) {
+    const skillCategory = props.skillCategory;
+    let alternateColor = true;
+
+    return (
+        <div className="skill-category">
+            <div className="category-title">
+                <div>
+                    { skillCategory.icon !== '' ? <i className={ skillCategory.icon }></i> : null }
+                    { skillCategory.name }
+                </div>
+            </div>
+            <div className="title-group">
+                { skillCategory.skillList.map(skill => (
+                    <div className={ alternateColor ? 'skill-title' : 'skill-title-alt'}>
+                        <div>
+                            { skill.icon !== '' ? <i className={ skill.icon }></i> : null }
+                            { skill.name }
+                        </div>
+                        <div>
+                            { skill.proficiency === 3 ? 'Advanced'
+                            : skill.proficiency === 2 ? 'Intermediary'
+                            : skill.proficiency === 1 ? 'Basic' 
+                            : null
+                            }
+                        </div>
+                        { alternateColor = !alternateColor }
+                    </div>
+                ))
+                }
+            </div>
+        </div>
+    )
+}
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,14 +1,88 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SkillSection from '../components/SkillSection';
 
 export default class Skills extends Component {
     state = {
-
+        skillCategory: [
+            {
+                name: 'Web Development',
+                icon: 'fas fa-file-code',
+                skillList: [
+                    {
+                        name: 'HTML',
+                        proficiency: 3,
+                        icon: '',
+                    },
+                    {
+                        name: 'CSS',
+                        proficiency: 3,
+                        icon: '',
+                    },
+                    {
+                        name: 'React.js',
+                        proficiency: 2,
+                        icon: '',
+                    },
+                    {
+                        name: 'Test',
+                        proficiency: 1,
+                        icon: '',
+                    },
+                ],
+            },
+            {
+                name: 'Tools',
+                icon: 'fas fa-wrench',
+                skillList: [
+                    {
+                        name: 'Photoshop',
+                        proficiency: 3,
+                        icon: '',
+                    },
+                    {
+                        name: 'CorelDraw',
+                        proficiency: 3,
+                        icon: '',
+                    },
+                    {
+                        name: 'Krita',
+                        proficiency: 2,
+                        icon: '',
+                    },
+                ],
+            },
+            {
+                name: 'Languages',
+                icon: 'fas fa-globe',
+                skillList: [
+                    {
+                        name: 'English',
+                        proficiency: 3,
+                        icon: '',
+                    },
+                ],
+            },
+        ],
+        experience: [
+            {
+                title: 'Database Assistant',
+                type: 'Intern',
+                company: 'Atac Distribuidora',
+                currentlyEmployed: false,
+                from: 2017,
+                to: 2018,
+                comment: 'Primary obligations were database maintenance and configuring the various systems in the company.',
+            },
+        ],
     }
 
     render() {
         return (
             <section className="skills-section">
-                
+                { this.state.skillCategory.map(skillCategory => (
+                    <SkillSection key={skillCategory.name} skillCategory={skillCategory}/>
+                ))
+                }
             </section>
         )
     }
