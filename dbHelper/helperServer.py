3b66c6307aa729c1165687d15e80f0ba31f8fe01
Vinesma <otaviocos_13@hotmail.com>
2020-01-07 10:27:45 -0300

    +Helper functions +findDocument()

--- dbHelper/helperServer.py
+++ dbHelper/helperServer.py
@@ -12,6 +12,9 @@ database = client.Portfolio
 collectionList = database.list_collection_names()
 count = 1
 
+def clearScreen():
+    os.system('clear' if sys.platform == 'linux' else 'cls')
+
 def presentChoice():
     chosenOption = sys.stdin.readline()
     try:
@@ -25,14 +28,19 @@ def presentChoiceString():
     chosenOption = sys.stdin.readline()
     return chosenOption.rstrip()
 
+def waitForInput():
+    input("\n Press Enter to continue...")
+    clearScreen()
+
 def listAllCollectionItems(collectionName):
     print("\n * Listing:")
     for item in database[collectionName].find({}, {"_id": 0, "__v": 0}):
         print("\n {}".format(item))
+    waitForInput()
 
 def insertDocument(collectionName):
     if collectionName == 'experiences':
-        print("\n * ADDING A NEW EXPERIENCE")
+        print("\n* ADDING A NEW EXPERIENCE")
         print(" * title:")
         title = presentChoiceString()
         print(" * type:")
@@ -47,10 +55,7 @@ def insertDocument(collectionName):
         dateTo = presentChoiceString()
         print(" * Are you currently employed at this job? (Y/N)")
         currentlyEmployed = presentChoiceString()
-        if currentlyEmployed.lower() == 'y':
-            currentlyEmployed = True
-        else:
-            currentlyEmployed = False
+        currentlyEmployed = True if currentlyEmployed.lower() == 'y' else False
         try:
             newExperience = {
                 "title" : title,
@@ -62,7 +67,7 @@ def insertDocument(collectionName):
                 "currentlyEmployed": currentlyEmployed,
             }
             database[collectionName].insert_one(newExperience)
-            print("\n * NEW EXPERIENCE ADDED!")
+            print("\n* NEW EXPERIENCE ADDED!")
         except Exception as err:
             print("\nERROR : {}".format(err))
     elif collectionName == 'skillcategories':
@@ -71,6 +76,22 @@ def insertDocument(collectionName):
         pass
     else:
         print("\nERROR : UNKNOWN COLLECTION NAME")
+    waitForInput()
+
+def findDocument(collectionName):
+    print("\n* Search for key:")
+    keySearchTerm = presentChoiceString().lower()
+    print("* Search for value:")
+    valueSearchTerm = presentChoiceString()
+    result = database[collectionName].find_one({ keySearchTerm : valueSearchTerm }, {"_id" : 0, "__v" : 0})
+
+    if result != None:
+        print("\n")
+        print(result)
+    else:
+        print("\nNo results found for your query...")
+    waitForInput()
+    return result
 
 # START
 
@@ -89,21 +110,21 @@ while True:
 
     choice = presentChoice()
     if choice == 1:
-        while choice != 5:
+        while choice != 6:
             print("\n-- WORK EXPERIENCE --\n")
             print("1. List all experiences.")
             print("2. Find an experience.")
             print("3. Add a new experience.")
             print("4. Modify an existing experience.")
             print("5. Remove an experience.")
-            print("6. Go back.")
+            print("6. Go back to all collections.")
             print("7. Exit program.")
 
             choice = presentChoice()
             if choice == 1:
                 listAllCollectionItems('experiences')
             elif choice == 2:
-                pass
+                findDocument('experiences')
             elif choice == 3:
                 insertDocument('experiences')
             elif choice == 4:
@@ -111,65 +132,66 @@ while True:
             elif choice == 5:
                 pass
             elif choice == 6:
-                pass
+                clearScreen()
             elif choice == 7:
                 print("Exiting...")
                 sys.exit()
             else:
                 print("\nERROR : Invalid option.")
     elif choice == 2:
-        while choice != 5:
+        while choice != 6:
             print("\n-- SKILL CATEGORIES --\n")
             print("1. List all skill categories.")
             print("2. Find a skill category.")
             print("3. Add a new skill category.")
             print("4. Modify an existing skill category.")
             print("5. Remove a skill category.")
-            print("6. Go back.")
+            print("6. Go back to all collections.")
             print("7. Exit program.")
 
             choice = presentChoice()
             if choice == 1:
-                pass
+                listAllCollectionItems('skillcategories')
             elif choice == 2:
-                pass
+                findDocument('skillcategories')
             elif choice == 3:
-                pass
+                insertDocument('skillcategories')
             elif choice == 4:
                 pass
             elif choice == 5:
                 pass
             elif choice == 6:
-                pass
+                clearScreen()
             elif choice == 7:
                 print("Exiting...")
                 sys.exit()
             else:
                 print("\nERROR : Invalid option.")
     elif choice == 3:
-        while choice != 5:
+        while choice != 6:
             print("\n-- IMAGE CATEGORIES --\n")
             print("1. List all image categories.")
-            print("2. Add a new image category.")
-            print("3. Modify an existing image category.")
-            print("4. Remove an image category.")
-            print("5. Go back.")
-            print("6. Exit program.")
+            print("2. Find an image category.")
+            print("3. Add a new image category.")
+            print("4. Modify an existing image category.")
+            print("5. Remove an image category.")
+            print("6. Go back to all collections.")
+            print("7. Exit program.")
 
             choice = presentChoice()
             if choice == 1:
-                pass
+                listAllCollectionItems('imagecategories')
             elif choice == 2:
-                pass
+                findDocument('imagecategories')
             elif choice == 3:
-                pass
+                insertDocument('imagecategories')
             elif choice == 4:
                 pass
             elif choice == 5:
                 pass
             elif choice == 6:
-                pass
-            elif choice == 6:
+                clearScreen()
+            elif choice == 7:
                 print("Exiting...")
                 sys.exit()
             else:
@@ -179,5 +201,3 @@ while True:
         sys.exit()
     else:
         print("\nERROR : Invalid option.")
-
-# print(db.skillcategories.find_one({ 'name': 'Tools' }))
